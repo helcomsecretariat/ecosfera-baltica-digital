4a5eb58e1d0dc6717e2c59b91aecae5f143e2462
Artem Sushchev <artem.sushchev@reaktor.com>
2024-10-08 23:38:01 +0200

    merged buy machine into turn machine

--- src/state/machines/turn.ts
+++ src/state/machines/turn.ts
@@ -1,12 +1,12 @@
 import { assign, setup, sendTo } from "xstate";
-import { BuyMachine, BuyMachineInEvents, BuyMachineOutEvents } from "@/state/machines/buy";
 import { AbilityMachineInEvents, AbilityMachineOutEvents, AbilityMachine } from "@/state/machines/ability";
-import { AbilityTile, AnimalCard, ElementCard, GameState, PlantCard } from "@/state/types";
+import { AbilityTile, AnimalCard, Card, ElementCard, GameState, PlantCard } from "@/state/types";
 import { DeckConfig } from "@/decks/schema";
 import { spawnDeck } from "@/state/deck-spawner";
 import { produce } from "immer";
 import { compact, countBy, entries, find, intersection, reject, without } from "lodash";
 import { replaceItem } from "@/state/utils";
+import { BuyMachineGuards } from "@/state/machines/guards/buy";
 
 export const TurnMachine = setup({
   types: {
@@ -21,12 +21,28 @@ export const TurnMachine = setup({
       | { type: "user.click.player.hand.card"; card: PlantCard | AnimalCard | ElementCard }
       | { type: "user.click.market.deck.element"; name: ElementCard["name"] }
       | { type: "user.click.market.table.card"; card: PlantCard | AnimalCard }
+      | { type: "iddqd"; context: GameState }
       | AbilityMachineInEvents
-      | AbilityMachineOutEvents
-      | BuyMachineOutEvents
-      | BuyMachineInEvents,
+      | AbilityMachineOutEvents,
   },
   actions: {
+    borrowElement: assign(({ context }: { context: GameState }, name: ElementCard["name"]) =>
+      produce(context, (draft) => {
+        const card = find(draft.elementMarket.deck, { name });
+        if (!card) return;
+        draft.turn.borrowedElement = card;
+      }),
+    ),
+    playCard: assign(({ context }: { context: GameState }, uid: Card["uid"]) =>
+      produce(context, (draft) => {
+        draft.turn.playedCards.push(uid);
+      }),
+    ),
+    unPlayCard: assign(({ context }: { context: GameState }, uid: Card["uid"]) =>
+      produce(context, (draft) => {
+        draft.turn.playedCards = without(draft.turn.playedCards, uid);
+      }),
+    ),
     buyCard: assign(({ context }: { context: GameState }, card: PlantCard | AnimalCard) =>
       produce(context, (draft) => {
         const { turn } = draft;
@@ -76,15 +92,31 @@ export const TurnMachine = setup({
         player.hand.push(card);
       }),
     ),
+
+    setContext: assign(({ context }, newContext: GameState) => ({ ...context, ...newContext })),
+  },
+  guards: {
+    notPlayedCard: BuyMachineGuards.notPlayedCard,
+    notExhausted: BuyMachineGuards.notExhausted,
+    canBorrow: BuyMachineGuards.canBorrow,
+    canBuyCard: BuyMachineGuards.canBuyCard,
   },
   actors: {
     ability: AbilityMachine,
-    buy: BuyMachine,
   },
 }).createMachine({
   id: "turn",
   context: ({ input: { config, numberOfPlayers, seed } }) => spawnDeck(config, numberOfPlayers, seed),
   initial: "buying",
+
+  on: {
+    iddqd: {
+      actions: {
+        type: "setContext",
+        params: ({ event: { context } }) => context,
+      },
+    },
+  },
 
   states: {
     buying: {
@@ -98,25 +130,36 @@ export const TurnMachine = setup({
             }),
           }),
         },
-        "user.click.*": {
-          actions: sendTo("buy", ({ event }) => event),
+        "user.click.market.deck.element": {
+          actions: { type: "borrowElement", params: ({ event: { name } }) => name },
+          guard: "canBorrow",
         },
-        "buy.card": {
-          reenter: true,
-          target: "buying",
+        "user.click.player.hand.card": [
+          {
+            actions: {
+              type: "playCard",
+              params: ({ event: { card } }) => card.uid,
+            },
+            guard: { type: "notPlayedCard", params: ({ event: { card } }) => card.uid },
+          },
+          {
+            actions: {
+              type: "unPlayCard",
+              params: ({ event: { card } }) => card.uid,
+            },
+            guard: {
+              type: "notExhausted",
+              params: ({ event: { card } }) => card.uid,
+            },
+          },
+        ],
+        "user.click.market.table.card": {
           actions: {
             type: "buyCard",
             params: ({ event: { card } }) => card,
           },
+          guard: { type: "canBuyCard", params: ({ event: { card } }) => card },
         },
-        "buy.iddqd": {
-          actions: assign({ turn: ({ event: { context } }) => context.turn }),
-        },
-      },
-      invoke: {
-        id: "buy",
-        src: "buy",
-        input: ({ context }) => ({ context }),
       },
     },
 

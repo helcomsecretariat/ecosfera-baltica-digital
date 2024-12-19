a65390e67cafb71aea882f0024b71915ff221409
Chris Dreyer <christiaanldreyer@gmail.com>
2024-12-19 14:01:39 +0100

    Fixed stage tile rendering #204
    Rebase stage tile rendering fixes

--- src/components/CenterTile.tsx
+++ src/components/CenterTile.tsx
@@ -4,13 +4,23 @@ import { useRelevantMaterial } from "@/components/MaterialProvider/hook";
 import { getAssetPath } from "./utils";
 import { useSRGBTexture } from "@/hooks/useSRGBTexture";
 import { toVector3 } from "@/utils/3d";
+import { useGameState } from "@/context/game-state/hook";
 
 const CenterTile = withMaterialProvider(
-  ({ texturePathSuffix, yStart }: { texturePathSuffix: string; yStart: number }) => {
+  ({ yStart, tileType }: { yStart: number; tileType: "habitat" | "extinction" }) => {
+    const { state } = useGameState();
     const { RelevantMaterial } = useRelevantMaterial();
-    const tileImageUrl = getAssetPath("tile", texturePathSuffix);
+    const tileImageUrl = getAssetPath("tile", tileType === "habitat" ? "baltic_active" : "extinction");
     const texture = useSRGBTexture(tileImageUrl);
-    const transform = tileGridTransforms(hexagonTileXStart, yStart)[6];
+
+    const showOnStage =
+      (state.stage?.eventType === "gameLoss" && tileType === "extinction") ||
+      (state.stage?.eventType === "gameWin" && tileType === "habitat");
+    const transform = tileGridTransforms(
+      showOnStage ? 0 : hexagonTileXStart,
+      showOnStage ? 5 + tileSize : yStart,
+      showOnStage ? 75 : 0,
+    )[6];
 
     return (
       <mesh
@@ -18,7 +28,7 @@ const CenterTile = withMaterialProvider(
         rotation={toVector3(transform?.rotation ?? { x: 0, y: 0, z: 0 })}
       >
         <cylinderGeometry args={[tileSize, tileSize, 0.1, 6, 1]} />
-        <RelevantMaterial map={texture} />
+        <RelevantMaterial map={texture} transparent />
       </mesh>
     );
   },
